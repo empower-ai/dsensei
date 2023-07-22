f263f3d5c56d40cd0c95246cfec8c0bf6db75908
Daiyi Yang <daiyiyang@MacBook-Pro-4.local>
2023-07-22 09:26:37 -0700

    Remove unused templating react files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { router } from "./router";
 import { store } from "./store";
 
@@ -18,8 +17,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
