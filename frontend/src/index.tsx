9ac586782156c5487ce61e33e412132ee2544590
Daiyi Yang <daiyiyang@MacBook-Pro-4.local>
2023-07-28 22:53:28 -0700

    Remove strict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
