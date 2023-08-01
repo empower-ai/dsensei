a294d5578b85688547eeab7cbad037ac33f73e00
Daiyi Yang <daiyiyang@MacBook-Pro-4.local>
2023-07-31 23:14:55 -0700

    Add loader for large file

--- frontend/src/routes/UploadCSV.tsx
+++ frontend/src/routes/UploadCSV.tsx
@@ -21,6 +21,7 @@ function CsvUploader() {
   const [header, setHeader] = useState<{ name: string; type: string }[]>([]);
   const [previewData, setPreviewData] = useState<{ [k: string]: string }[]>([]);
   const [error, setError] = useState<string>("");
+  const [isProcessFile, setIsProcessingFile] = useState<boolean>(false);
   const [fileLoaded, setFileLoaded] = useState<boolean>(false);
 
   const db = rd.useDuckDB();
@@ -45,6 +46,7 @@ function CsvUploader() {
       header: true,
       delimiter: ",",
     });
+    setIsProcessingFile(false);
     setFileLoaded(true);
 
     const res = await conn.query("DESCRIBE uploaded_content");
@@ -75,6 +77,7 @@ function CsvUploader() {
   };
 
   const onDrop = async <T extends File>(acceptedFiles: T[]) => {
+    setIsProcessingFile(true);
     const fileReader = new FileReader();
 
     fileReader.onload = async function (event) {
@@ -119,38 +122,48 @@ function CsvUploader() {
             )
           </Text>
           <Card className="max-w-6xl">
-            <div {...getRootProps()}>
-              <div className="flex items-center w-full justify-center">
-                <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Click to upload</span> or
-                      drag and drop the csv file here.
-                    </p>
-                  </div>
-                  <input type="file" {...getInputProps()} />
-                </label>
-              </div>
-            </div>
-            {error && (
-              <Flex justifyContent="center" className="pt-5">
-                <Text color="red">{error}</Text>
+            {isProcessFile && (
+              <Flex className="h-64	gap-3" justifyContent="center">
+                <p>Processing</p>
+                <span className="loading loading-bars loading-lg"></span>
               </Flex>
+            )}
+            {!isProcessFile && !fileLoaded && (
+              <>
+                <div {...getRootProps()}>
+                  <div className="flex items-center w-full justify-center">
+                    <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg
+                          className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 20 16"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                          />
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          <span className="font-semibold">Click to upload</span>{" "}
+                          or drag and drop the csv file here.
+                        </p>
+                      </div>
+                      <input type="file" {...getInputProps()} />
+                    </label>
+                  </div>
+                </div>
+                {error && (
+                  <Flex justifyContent="center" className="pt-5">
+                    <Text color="red">{error}</Text>
+                  </Flex>
+                )}
+              </>
             )}
           </Card>
         </>
@@ -163,6 +176,8 @@ function CsvUploader() {
             setFile(undefined);
             setPreviewData([]);
             setHeader([]);
+            setFileLoaded(false);
+            db.value?.reset();
           }}
           header={header}
           previewData={previewData}
